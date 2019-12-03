f3476a89f2b1c647f226ed8f08798d5338034d77
xroota <xrootb@gmail.com>
2019-12-03 21:49:21 +0300

    Добавлен интерфейс ModelData

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -1,8 +1,20 @@
-class Model {
-    private min: number;
-    private max: number;
-    private value: number;
-    private values: Array<number>;
-    private isRange: boolean;
-
-}
+interface ModelData {
+    min: number,
+    max: number,
+    value?: number,
+    values?: Array<number>,
+    isRange?: boolean
+  }
+  
+  
+  
+  class Model {
+    private modelData :  ModelData = {
+      min: 100,
+      max: 300,
+      value: 250,
+      isRange: false,
+    }
+  
+  }
+export {Model};  
