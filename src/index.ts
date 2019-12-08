51bd710e01b38431557a247c1b136299f5049666
xroota <xrootb@gmail.com>
2019-12-08 19:34:43 +0300

    Добавлена конфигурация слацдера, data.gui

--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,13 @@
  * limitations under the License.
  */
 import '../dist/index.scss';
-import {Car} from './Car';
 import {View} from './View/View';
 import {Model} from './Model/Model';
 import {Config} from './View/View';
 import {Presenter} from './Presenter/Presenter';
+import * as dat from 'dat.gui';
 
 
-console.log('hi');
-const car: Car = new Car(); 
-car.go('vroom');
 const model: Model = new Model();
 
 
@@ -33,5 +30,82 @@ const view = new View("input", Object.assign({}, config, model.modelData));
 
 const presenter = new Presenter(model,view);
 
-//view.show('hi, world!');
-//view.show('hi, world!');
+var gui = new dat.GUI({ autoPlace: false });
+
+var customContainer = document.getElementById('my-gui-container');
+customContainer.appendChild(gui.domElement);
+var customContainer = document.getElementById('my-gui-container');
+customContainer.appendChild(gui.domElement);
+const f1 = gui.addFolder("Config");
+const ft = gui.addFolder("Tooltips");
+//const f2 = gui.addFolder("Methods");
+const f3 = gui.addFolder("Color");
+const steps = {
+	"0.001": 0.001,
+	"0.01": 0.01,
+	"0.1": 0.1,
+	"1": 1,
+	"1.25": 1.25,
+	"2.0": 2.0,
+	"2.5": 2.5,
+	"5.0": 5.0,
+	"10.0": 10.0
+};
+
+f1.add(config, "min").min(0).step(1).max(1000).onChange(updateConfig);
+f1.add(config, "max").min(0).step(1).max(1000).onChange(updateConfig);
+f1.add(config, "step", steps).onChange(updateConfig);
+f1.add(config, "vertical").onChange(updateConfig);
+f1.add(config, "multiple").onChange(updateConfig);
+
+
+ft.add(config, "tooltip").onChange(updateConfig);
+ft.add(config, "showTooltips").name("Always show?").onChange(updateConfig);
+
+
+f1.open();
+ft.open();
+
+//f2.add(config, "init");
+//f2.add(config, "destroy");
+
+
+f3.addColor(config, 'color1').name('Primary').onChange(val => {
+	document.body.style.setProperty("--primary", val);
+});
+f3.addColor(config, 'color2').name('Secondary').onChange(val => {
+	document.body.style.setProperty("--secondary", val);
+});
+
+//f2.open();
+f3.open();
+
+function updateConfig(val) {
+	const prop = this.property;
+	
+	if ( prop !== "tooltip" ) {
+		view.input[prop] = val;
+	} else {
+		view.nodes.container.classList.toggle("has-tooltip", val);
+	}
+	
+	if ( prop === "showTooltips" ) {
+		view.nodes.container.classList.toggle("show-tooltip", val);
+	}
+	
+	config[prop] = val;
+	view.config[prop] = val;	
+
+	if ( prop === "step" ) {
+		view.setValue();
+	}
+	
+	if ( prop === "vertical" || prop === "multiple" ) {
+		view.destroy();
+		setTimeout(() => {
+			view.init();
+		}, 10);
+	} else {
+		view.update();
+	}
+}
