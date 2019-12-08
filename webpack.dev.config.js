7d410a0bb94c05851c851eb2dfd6c048f6ad7508
xroota <xrootb@gmail.com>
2019-12-08 10:45:40 +0300

    Добален SASS

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -34,6 +34,17 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ],
   },
   resolve: {
