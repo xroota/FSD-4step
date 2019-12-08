c4a93740cee63ce6c954fb60093bcc4c99ef6c2f
xroota <xrootb@gmail.com>
2019-12-08 19:32:29 +0300

    Добавил события на уведомления Observer

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -1,15 +1,35 @@
+import { Model } from "../Model/Model";
+import { View } from "../View/View";
+
+
 class Presenter {
+    model: Model;
+    view: View;
+    
     constructor(model,view){
-        view.eventObserver.addObserver(this.dataChange.bind(this));
-
+        view.eventObserver.addObserver(this.dataViewChange.bind(this));
+        model.eventObserver.addObserver(this.dataModelChange.bind(this));
+        this.model= model;
+        this.view= view;
     }
-    dataChange(data){
+    
+
+
+    dataViewChange(data){
         if (data.message==="valueChange"){
-            alert(data.message + ' ' + data.value)
+            alert(data.message + ' ' + data.value);
+            this.model.setValue(data.value);
         }
-        
-
     }
+
+    
+    dataModelChange(data){
+        if (data.message==="valueChange"){
+            alert(data.message + ' ' + data.value);
+            this.view.setValue(data.value);
+        }
+    }
+        
 
 }
 export {Presenter}
