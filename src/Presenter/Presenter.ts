527559f94926d734417caba695eadd9a2d382c8d
xroota <xrootb@gmail.com>
2019-12-16 16:17:45 +0300

    Плагин JQury

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -1,12 +1,12 @@
 import { Model } from "../Model/Model";
 import { View } from "../View/View";
-import { EventObserver, EventData} from "../EventObserver/EventObserver";
+import { EventObserver, EventData } from "../EventObserver/EventObserver";
 
 class Presenter {
     model: Model;
     view: View;
 
-    constructor(model:Model, view:View ) {
+    constructor(model: Model, view: View) {
         view.eventObserver.addObserver(this.dataViewChange.bind(this));
         model.eventObserver.addObserver(this.dataModelChange.bind(this));
         this.model = model;
@@ -15,20 +15,20 @@ class Presenter {
 
 
 
-    dataViewChange(data:EventData):void {
+    dataViewChange(data: EventData): void {
         if (data.message === "valueChange") {
             //alert(data.message + ' ' + data.value);
             if (Array.isArray(data.value))
-            this.model.setProperty("value", data.value);
+                this.model.setProperty("value", data.value);
         }
     }
 
-    dataModelChange(data:EventData) :void  {
+    dataModelChange(data: EventData): void {
         if (data.message === "change") {
             //alert(data.message + ' ' + data.value);
             switch (data.property) {
                 case "value": this.view.setValue(data.value[0], 0);
-                    if ( Array.isArray(data.value) && data.value.length > 0) this.view.setValue(data.value[1], 1);
+                    if (Array.isArray(data.value) && data.value.length > 0) this.view.setValue(data.value[1], 1);
                     $("#range-value").text(data.value.toString);
                     break;
                 default: this.view.updateConfig(data.property, data.value);
--- src/index.ts
+++ src/index.ts
@@ -14,27 +14,182 @@
  * limitations under the License.
  */
 import '../dist/index.scss';
-import { View } from './View/View';
-import { Model } from './Model/Model';
-import { Config } from './View/View';
+import { View, ViewConfig } from './View/View';
+import { Model, ModelData } from './Model/Model';
+
 import { Presenter } from './Presenter/Presenter';
 import * as dat from 'dat.gui';
 
 
-const model: Model = new Model();
 
 
-let config = new Config();
 
-const view = new View("input", config = Object.assign({}, config, model.modelData));
+
+
+
+
+let configSlider = Object.assign({}, {
+    tooltip: true,
+    vertical: false,
+    showTooltips: true,
+    color1: "#98890c",
+    color2: "#431010",
+}, { min: 0, max: 10000, step: 10, value: [2000, 4000] });
+
+let modelConfig = new ModelData();
+
+let viewConfig = new ViewConfig();
+
+for (let key in configSlider) {
+    if (key in modelConfig) {
+        modelConfig[key] = configSlider[key];
+    }
+    else viewConfig[key] = configSlider[key];
+}
+
+
+const model = new Model(modelConfig);
+
+const view = new View($(".slider88"), Object.assign({}, model.modelData, viewConfig));
 const presenter = new Presenter(model, view);
+
+
+
+
+
+
+function updateModel(val) {
+    const prop = this.property;
+    if (prop === 0 || prop === 1) {
+        model.setValue(prop, val);
+    }
+    else
+        model.setProperty(prop, val);
+}
+
+function updateView(val) {
+    const prop = this.property;
+    //  view.updateConfig(prop, val);
+}
+
+
+module Slider {
+
+
+
+    export class Init {
+
+        element: JQuery;
+        options: object;
+        model: Model;
+
+
+        constructor(element: JQuery, options: object) {
+
+            this.element = element;
+            this.options = options;
+
+            let modelConfig = new ModelData();
+            let viewConfig = new ViewConfig();
+
+            for (let key in options) {
+                if (key in modelConfig) {
+                    modelConfig[key] = options[key];
+                }
+                else viewConfig[key] = options[key];
+            }
+
+
+            this.model = new Model(modelConfig);
+
+            const view = new View(element, Object.assign({}, this.model.modelData, viewConfig));
+            const presenter = new Presenter(this.model, view);
+            this.OnCreate();
+
+
+        }
+
+
+        OnCreate() {
+            //this.element.css('color', this.options.color).css('background-color', this.options.backgroundColor);
+        }
+        getValue() {
+            return this.model.modelData.value;
+        }
+
+    }
+
+
+
+
+}
+
+
+declare global {
+    interface Window {
+        $: JQuery;
+    }
+    interface JQuery {
+        Slider: (
+            opts?: object,
+        ) => JQuery<Element> | string | number | number[] | boolean;
+
+    }
+}
+
+//jquery plugin wrapper.
+(function (w, $) {
+    if (!$) return false;
+
+    $.fn.extend({
+        Slider: function (opts) {
+
+            //defaults
+            //var defaults: Coloring.GreenifyOptions = new Coloring.GreenifyOptions('#0F0', '#000');
+
+            //var opts = $.extend(defaults, opts);
+
+            return this.each(function () {
+                var o = opts;
+                var obj = $(this);
+                new Slider.Init(obj, o);
+
+            });
+        }
+    });
+})(window, jQuery);
+
+let configSlider2 = Object.assign({}, {
+    tooltip: true,
+    vertical: false,
+    showTooltips: true,
+    color1: "#98890c",
+    color2: "#431010",
+}, { min: 0, max: 12000, step: 10, value: [2000, 7000] });
+
+let configSlider3 = Object.assign({}, {
+    tooltip: true,
+    vertical: false,
+    showTooltips: true,
+    color1: "#98890c",
+    color2: "#431010",
+}, { min: 0, max: 20000, step: 10, value: [2000] });
+
+
+
+$(".slider7").Slider(configSlider3);
+$(".slider2").Slider(configSlider2);
+var customValue: any = document.querySelector(".slider2 .slider__input");
+
+var ss = customValue["value"];
+var dd = 11;
 
 var gui = new dat.GUI({ autoPlace: false });
 
+
 var customContainer = document.getElementById('my-gui-container');
 customContainer.appendChild(gui.domElement);
-var customContainer = document.getElementById('my-gui-container');
-customContainer.appendChild(gui.domElement);
+
 const f1 = gui.addFolder("Config");
 const ft = gui.addFolder("Tooltips");
 const f2 = gui.addFolder("Values");
@@ -51,14 +206,17 @@ const steps = {
     "10.0": 10.0
 };
 
+
+
+var dd = customContainer["min"];
 f1.add(view.config, "min").step(1).onChange(updateModel);
 f1.add(view.config, "max").step(1).onChange(updateModel);
 f1.add(view.config, "step", steps).onChange(updateModel);
 f1.add(view.config, "vertical").onChange(updateView);
 f1.add(view.config, "multiple").onChange(updateModel);
 
-f2.add(model.modelData.value, 0).onChange(updateModel).listen();
-f2.add(model.modelData.value, 1).onChange(updateModel).listen();
+f2.add(customValue,"values").onChange(updateModel).listen();
+f2.add(customValue.value, 1).onChange(updateModel).listen();
 
 ft.add(view.config, "tooltip").onChange(updateView);
 ft.add(view.config, "showTooltips").name("Always show?").onChange(updateView);
@@ -67,27 +225,12 @@ f1.open();
 ft.open();
 
 
-f3.addColor(config, 'color1').name('Primary').onChange(val => {
+f3.addColor(viewConfig, 'color1').name('Primary').onChange(val => {
     document.body.style.setProperty("--primary", val);
 });
-f3.addColor(config, 'color2').name('Secondary').onChange(val => {
+f3.addColor(viewConfig, 'color2').name('Secondary').onChange(val => {
     document.body.style.setProperty("--secondary", val);
 });
 
 f2.open();
 f3.open();
-
-
-function updateModel(val) {
-    const prop = this.property;
-    if (prop === 0 || prop === 1) {
-        model.setValue(prop, val);
-    }
-    else
-        model.setProperty(prop, val);
-}
-
-function updateView(val) {
-    const prop = this.property;
-    view.updateConfig(prop, val);
-}
