b4796e317950e002c1de65639c124100d48d9bba
xroota <xrootb@gmail.com>
2019-12-18 00:37:30 +0300

    Добавлены функции изменения модели и вью, функция вывода значенияы в элемент

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -5,12 +5,16 @@ import { EventObserver, EventData } from "../EventObserver/EventObserver";
 class Presenter {
     model: Model;
     view: View;
-
-    constructor(model: Model, view: View) {
+    outputElement: JQuery;
+    constructor(model: Model, view: View, outputElement?: string) {
         view.eventObserver.addObserver(this.dataViewChange.bind(this));
         model.eventObserver.addObserver(this.dataModelChange.bind(this));
         this.model = model;
         this.view = view;
+        this.outputElement = $(outputElement);
+        this.setOutput();
+        $(this.outputElement).change( this.setModelValue.bind(this));
+
     }
 
 
@@ -29,14 +33,46 @@ class Presenter {
             switch (data.property) {
                 case "value": this.view.setValue(data.value[0], 0);
                     if (Array.isArray(data.value) && data.value.length > 0) this.view.setValue(data.value[1], 1);
-                    $("#range-value").text(data.value.toString);
+                    this.setOutput();
                     break;
                 default: this.view.updateConfig(data.property, data.value);
             }
         }
 
 
+
     }
+    setOutput() {
+        let value = this.model.modelData.value;
+        if (this.outputElement.is("input")) {
+            this.outputElement.val(value[1] ? (value as number[]).join("-") : value[0]);
+        }
+        else {
+            this.outputElement.text(value[1] ? (value as number[]).join("-") : value[0]);
+        }
+
+    }
+
+    setModelValue(){
+        let val:string    = $(this.outputElement).val().toString();
+        let valArr: number[] =val.split("-").map(Number);
+        this.model.setProperty("value", valArr);
+
+
+    }
+
+    setProperty(prop: string, value: number | number[] | string): void {
+        this.model.setProperty
+            if (prop in this.model.modelData) {
+                this.model.setProperty(prop,value);
+            }
+            else {
+                this.view.updateConfig(prop,value);
+            }
+
+
+    }
+
 
 }
 export { Presenter }
