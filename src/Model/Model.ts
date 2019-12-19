7e8eb37ce86f8f6fb5f7bc843770f0b22ebbdc3f
xroota <xrootb@gmail.com>
2019-12-19 15:56:26 +0300

    Добавлена логика модели. Фукция запроса параметров

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -23,27 +23,53 @@ class Model {
   constructor(data: ModelData) {
     this.modelData = data;
     this.checkMultiple();
+    this.checkValue();
   }
  
   checkMultiple(){
     this.modelData.multiple = this.modelData.value.length > 1 ? true : false;
 
   }
+  checkValue(){
+   
+    if(this.modelData.value[0] > this.modelData.max ) {this.modelData.value[0] = this.modelData.max};
+    if(this.modelData.value[0] < this.modelData.min ) {this.modelData.value[0] = this.modelData.min};
+    if (this.modelData.multiple){
+      if(this.modelData.value[1] > this.modelData.max ) {this.modelData.value[1] = this.modelData.max};
+      if(this.modelData.value[1] < this.modelData.min ) {this.modelData.value[1] = this.modelData.min}; 
+    }
+
+  }
 
   setProperty(prop: string, value: number | number[] | string): void {
+    let val= this.modelData.value;
     this.modelData[prop] = value;
     if (prop === "value")  {
       this.checkMultiple();
+    }  
+
+    if (prop === "multiple")  {
+      if ((typeof value ==="boolean") && (value === true))  {
+        this.modelData.value = [this.modelData.value[0],this.modelData.max];
+      }
+      else {
+        this.modelData.value = [this.modelData.value[0]];
+      }
+      
+
+    }  
+    
+    this.checkValue();
+    if (val !== this.modelData.value) {
+      this.eventObserver.notifyObservers({ message: "change", property:"value", value: this.modelData.value });
     }
     this.eventObserver.notifyObservers({ message: "change", property: prop, value: this.modelData[prop] });
+
+
   }
-  setValue(index: number, value: number): void {
-    this.modelData.value[index] = value;
-    this.eventObserver.notifyObservers({ message: "change", property: "value", value: this.modelData.value });
+  getProperty(prop:string){
+    return this.modelData[prop];
   }
-
-
-
 
 
 }
