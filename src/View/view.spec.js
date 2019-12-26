3c4ee20db89fcd119fe91fbcf9a718ef1492a804
xroota <xrootb@gmail.com>
2019-12-26 22:36:13 +0300

    Тесты view закончены

--- src/View/view.spec.js
+++ src/View/view.spec.js
@@ -1,4 +1,6 @@
 import { View, ViewConfig } from "../View/View";
+import { Model } from "../Model/Model";
+import { Presenter } from "../Presenter/Presenter";
 
 describe("View", () => {
   describe("constructor()", () => {
@@ -21,7 +23,7 @@ describe("View", () => {
       });
     });
 
-    it("может быть созан", () => {
+    it("может быть создан", () => {
       expect(view).toBeDefined();
     });
 
@@ -161,7 +163,7 @@ describe("View", () => {
   });
 
   describe("setValueFromPosition()", () => {
-    it("должен менять значение value при клике мыши", () => {
+    it("может менять значение value при клике мыши", () => {
       let view;
       setFixtures('<div class="slider"></div> <input id="output">');
       view = new View($(".slider"), {
@@ -220,7 +222,7 @@ describe("View", () => {
       });
     });
 
-    it("должен устанавливать классы при нажатой кнопке мыши", () => {
+    it("может устанавливать классы при нажатой кнопке мыши", () => {
       var evt = document.createEvent("MouseEvents");
       evt.initMouseEvent(
         "click",
@@ -242,14 +244,520 @@ describe("View", () => {
       view.down(evt);
       expect($(".slider__container")).toHaveClass("slider__handle--dragging");
       expect($(".slider__handle")).toHaveClass("slider__handle--active");
-      var clickEvents = $(document);
-      console.log(getEventListeners(document));
-        jQuery.each(clickEvents, function(key, handlerObj) {
-            
+    });
+  });
 
-      
-        });
+  describe("move()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: false,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 10,
+        value: [2000]
+      });
+    });
 
+    it("может менять значение при перемещении мыши", () => {
+      var evt = document.createEvent("MouseEvents");
+      evt.initMouseEvent(
+        "click",
+        true,
+        true,
+        window,
+        0,
+        0,
+        0,
+        142, // координаты x
+        0, // координаты y
+        false,
+        false,
+        false,
+        false,
+        0,
+        null
+      );
+      view.down(evt);
+
+      expect(view.config.value).toEqual([3000]);
+
+      evt.initMouseEvent(
+        "mousemove",
+        true,
+        true,
+        window,
+        0,
+        0,
+        0,
+        187, // координаты x
+        0, // координаты y
+        false,
+        false,
+        false,
+        false,
+        0,
+        null
+      );
+      view.move(evt);
+
+      expect(view.config.value).toEqual([4000]);
+
+      expect($(".slider__container")).toHaveClass("slider__handle--dragging");
+      expect($(".slider__handle")).toHaveClass("slider__handle--active");
+    });
+  });
+
+  describe("up()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: false,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 10,
+        value: [2000]
+      });
+    });
+
+    it("может удалать классы", () => {
+      var evt = document.createEvent("MouseEvents");
+      evt.initMouseEvent(
+        "click",
+        true,
+        true,
+        window,
+        0,
+        0,
+        0,
+        142, // координаты x
+        0, // координаты y
+        false,
+        false,
+        false,
+        false,
+        0,
+        null
+      );
+      view.down(evt);
+
+      expect(view.config.value).toEqual([3000]);
+      expect($(".slider__container")).toHaveClass("slider__handle--dragging");
+      expect($(".slider__handle")).toHaveClass("slider__handle--active");
+
+      evt.initMouseEvent(
+        "mouseup",
+        true,
+        true,
+        window,
+        0,
+        0,
+        0,
+        187, // координаты x
+        0, // координаты y
+        false,
+        false,
+        false,
+        false,
+        0,
+        null
+      );
+
+      view.move(evt);
+      view.up();
+
+      expect($(".slider__container")).not.toHaveClass(
+        "slider__handle--dragging"
+      );
+      expect($(".slider__handle")).not.toHaveClass("slider__handle--active");
+      expect(view.config.value).toEqual([4000]);
+    });
+  });
+
+  describe("recalculate()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: false,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 10,
+        value: [2000]
+      });
+    });
+
+    it("может сохранять DOMRect hanle и container", () => {
+      view.rects = {};
+
+      view.recalculate();
+      var obj = $.extend({}, view.rects.handle);
+      delete obj.toJSON;
+      expect(obj).toEqual({
+        x: 8,
+        y: 35,
+        width: 89.59375,
+        height: 18,
+        top: 35,
+        right: 97.59375,
+        bottom: 53,
+        left: 8
+      });
+      obj = $.extend({}, view.rects.container);
+      delete obj.toJSON;
+      expect(obj).toEqual({
+        x: 8,
+        y: 8,
+        width: 896,
+        height: 45,
+        top: 8,
+        right: 904,
+        bottom: 53,
+        left: 8
+      });
+    });
+  });
+
+  describe("change()", () => {
+    let model;
+    let view;
+    let presenter;
+    let modelConfig;
+    let viewConfig;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+
+      modelConfig = {
+        min: 1000,
+        max: 2000,
+        step: 10,
+        value: [1500]
+      };
+
+      viewConfig = {
+        tooltip: true,
+        vertical: false,
+        showTooltips: true
+      };
+      model = new Model(modelConfig);
+
+      view = new View(
+        $(".slider"),
+        Object.assign({}, model.modelData, viewConfig)
+      );
+      presenter = new Presenter(model, view, "#output");
+    });
+
+    it("может отравлять value подписанной на событие model", () => {
+      presenter.view.config.value = [1700];
+      presenter.view.change();
+      expect(presenter.model.modelData.value).toEqual([1700]);
+    });
+  });
+
+  describe("update()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 5000]
+      });
+    });
+
+    it("может станавливать значение accuracy", () => {
+      expect(view.accuracy).toEqual(1);
+    });
+  });
+
+  describe("setValue()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 12000]
+      });
+    });
+
+    it("может устанавливать значение value", () => {
+      view.setValue([2500], 0);
+      expect(view.config.value[0]).toEqual(2500);
+      view.setValue([5500], 1);
+      expect(view.config.value[1]).toEqual(5500);
+    });
+
+    it("может устанавливать значение текста tooltip", () => {
+      view.setValue([2500], 0);
+      expect($(".slider__tooltip")[0]).toHaveText("2500.0");
+      view.setValue([5500], 1);
+      expect($(".slider__tooltip")[1]).toHaveText("5500.0");
+    });
+
+    it("может устанавливать класс combinedTooltip", () => {
+      view.setValue([2500], 0);
+      view.setValue([2600], 1);
+      expect($(".slider__container")).toHaveClass("slider--combined-tooltip");
+    });
+  });
+
+  describe("setPosition()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 12000]
+      });
+    });
+
+    it("может устанавливать значение css slider__progress", () => {
+      expect($(".slider__progress")).toHaveCss({
+        left: "89.6px",
+        width: "448px"
+      });
+    });
+  });
+
+  describe("getPosition()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 12000]
+      });
+    });
+
+    it("может получать длинну slider__progress", () => {
+      expect(view.getPosition(6000)).toEqual(268.8);
+    });
+  });
+
+  describe("tipsIntersecting()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures(
+        '@import "../jquery.slider.scss";<div style="width: 300px; height: 400px" class="slider" ></div> <input id="output">'
+      );
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 4000]
+      });
+    });
+
+    it("может вычислять нужно ли оъединять текст tooltip", () => {
+      expect(view.tipsIntersecting()).toEqual(true);
+    });
+  });
+
+  describe("getHandle()", () => {
+    it("может возвращать handle который нажат, или находитя ближе", () => {
+      let view;
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: false,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 10,
+        value: [2000]
+      });
+
+      var evt = document.createEvent("MouseEvents");
+      evt.initMouseEvent(
+        "click",
+        true,
+        true,
+        window,
+        0,
+        0,
+        0,
+        142, // координаты x
+        0, // координаты y
+        false,
+        false,
+        false,
+        false,
+        0,
+        null
+      );
+      let handle = view.getHandle(evt);
+      expect(handle).toExist();
+    });
+  });
+
+  describe("destroy()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 4000]
+      });
+    });
+
+    it("может удалять DOM элементы слайдера", () => {
+      expect($(".slider__container")).toExist();
+
+      view.destroy();
+
+      expect($(".slider__container")).not.toExist();
+    });
+
+    it("может удалять объект слайдера", () => {
+      expect(view.input.ranger).toExist();
+
+      view.destroy();
+
+      expect(view.input.ranger).not.toExist();
+    });
+  });
+
+  describe("bind()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 4000]
+      });
+    });
+
+    it("может вешать события", () => {
+      view.unbind();
+      view.bind();
+      expect($(".slider__input")).toHandle("change");
+      expect($(document)).toHandle("scroll");
+      expect($(window)).toHandle("resize");
+      expect($(".slider__container")).toHandle("mousedown");
+    });
+  });
+
+  describe("unbind()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 4000]
+      });
+    });
+
+    it("может удалять события", () => {
+      view.unbind();
+      expect($(".slider__input")).not.toHandle("change");
+      expect($(document)).not.toHandleWith("scroll");
+      expect($(window)).not.toHandleWith("resize");
+      expect($(".slider__container")).not.toHandle("mousedown");
+    });
+  });
+
+  describe("throttle()", () => {
+    let view;
+    beforeEach(() => {
+      setFixtures('<div class="slider"></div> <input id="output">');
+      view = new View($(".slider"), {
+        tooltip: true,
+        showTooltips: true,
+        vertical: false,
+        multiple: true,
+        color1: "#3db13d",
+        color2: "#ccc",
+        min: 0,
+        max: 20000,
+        step: 0.1,
+        value: [2000, 4000]
+      });
+    });
+
+    it("возвращает функцию", () => {
+      expect(view.throttle(function() {})).toBeInstanceOf(Function);
     });
   });
 });
